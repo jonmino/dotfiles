cc7997f7da6ba31fd8e3483fc849df33be53fbee
Jonas Minor <github@nc.seltsam.net>
2024-01-13 05:35:53 +0100

    First full symlink Script
    Finshed the script to symlink files and put some files in the correct place

--- symlink_conf.zsh
+++ symlink_conf.zsh
@@ -3,7 +3,7 @@
 
 set -eu -o pipefail # fail on error and report it, debug all lines
 
-echo "Creating symbolic links for all directories in ./config"
+echo "Creating symbolic links for all directories in ./.config"
 
 ls .config/ | {
     declare -a dirlist
@@ -12,3 +12,22 @@ ls .config/ | {
         cp -rsf ~/dotfiles/.config/"${dirlist}"/. ~/.config/"${dirlist}"
     done 
 }
+
+echo "Creating symbolic links for all directories in ./.oh-my-zsh"
+
+ls .oh-my-zsh/ | {
+    declare -a dirlist
+    while read dirlist;
+    do
+        cp -rsf ~/dotfiles/.oh-my-zsh/"${dirlist}"/. ~/.oh-my-zsh/"${dirlist}"
+    done 
+}
+
+echo "Creating symbolic links for chosen files in ./"
+
+files=(.p10k.zsh .zshenv .zshrc .gitconfig .condarc)
+for file in $files; do
+    ln -sf ~/dotfiles/$file ~/$file
+done
+
+echo "Finished executing the Script"
