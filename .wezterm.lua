3aa14f25408cab44feb148601828aed8c5c039f5
Jonas Minor <github@nc.seltsam.net>
2024-01-19 12:26:01 +0100

    Better looking Tabs
    First implementation of Tabs like in the old tmux config. Still need some adjusting

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,11 +13,27 @@ end
 -- imports/aliases
 local mux = wezterm.mux
 local act = wezterm.action
--- Triangle left: "\uE0B0" -> 
--- Triangle right: "\uE0B2" -> 
--- Slash left: "\ue0ba" -> 
--- Slash right: "\ue0bc" -> 
+-- Colours
+local BASE = "#1e1e2e"
+local CRUST = "#11111b"
+local SURFACE0 = "#313244"
+local SURFACE1 = "#45475a"
+local TEXT = "#cdd6f4"
+local SUBTEXT0 = "#a6adc8"
+local PEACH = "#fab387"
+local SAPPHIRE = "#74c7ec"
+local SKY = "#89dceb"
+-- Symbols
+-- The filled in variant of the (\uE0B0) symbol
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+-- The filled in variant of the (\uE0B2) symbol
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
 -- Rectangle: "\u2588" -> █
+local SOLID_RECTANGLE = "█"
+-- Slash left: "\ue0ba" -> 
+local SOLID_SLASH_LEFT = ""
+-- Slash right: "\ue0bc" -> 
+local SOLID_SLASH_RIGHT = ""
 
 -- Basic Settings:
 -- change default domain to WSL
@@ -52,6 +68,94 @@ config.integrated_title_button_style = "Windows" -- Styles = Windows, MacOSNativ
 config.use_fancy_tab_bar = false
 config.status_update_interval = 1000
 config.tab_bar_at_bottom = false
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+function tab_title(tab_info)
+	local title = tab_info.tab_title
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
+	end
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tab_info.active_pane.title
+end
+
+function tab_number(tab_info)
+	local number = tostring(tab_info.tab_index + 1)
+	if number and #number > 0 then
+		return number
+	end
+end
+
+wezterm.on(
+	'format-tab-title',
+	function(tab, tabs, panes, config, hover, max_width)
+		local edge_background = CRUST
+
+		if tab.is_active then
+			background = BASE
+			foreground = PEACH
+		elseif hover then
+			background = SURFACE1
+			foreground = SKY
+		else
+			background = CRUST
+			foreground = SAPPHIRE
+		end
+
+		local edge_foreground = background
+
+		local title = tab_title(tab)
+		local number = tab_number(tab)
+		-- ensure that the titles fit in the available space,
+		-- and that we have room for the edges.
+		title = wezterm.truncate_right(title, max_width - 4)
+		number = wezterm.truncate_right(number, max_width - 4)
+
+		return {
+			{ Background = { Color = edge_background } },
+			{ Foreground = { Color = edge_foreground } },
+			{ Text = SOLID_SLASH_LEFT },
+			{ Background = { Color = background } },
+			{ Foreground = { Color = foreground } },
+			{ Text = title },
+			{ Background = { Color = foreground } },
+			{ Foreground = { Color = background } },
+			{ Text = SOLID_SLASH_RIGHT },
+			{ Text = number },
+			{ Background = { Color = background } },
+			{ Foreground = { Color = foreground } },
+			{ Text = SOLID_SLASH_RIGHT },
+		}
+	end
+)
+config.tab_bar_style = {
+	new_tab = wezterm.format {
+		{ Background = { Color = BASE } },
+		{ Foreground = { Color = SURFACE0 } },
+		{ Text = SOLID_SLASH_LEFT },
+		{ Background = { Color = SURFACE0 } },
+		{ Foreground = { Color = SUBTEXT0 } },
+		{ Text = "+" },
+		{ Background = { Color = CRUST } },
+		{ Foreground = { Color = SURFACE0 } },
+		{ Text = SOLID_SLASH_RIGHT },
+	},
+	new_tab_hover = wezterm.format {
+		{ Background = { Color = BASE } },
+		{ Foreground = { Color = SURFACE1 } },
+		{ Text = SOLID_SLASH_LEFT },
+		{ Background = { Color = SURFACE1 } },
+		{ Foreground = { Color = TEXT } },
+		{ Text = "+" },
+		{ Background = { Color = CRUST } },
+		{ Foreground = { Color = SURFACE1 } },
+		{ Text = SOLID_SLASH_RIGHT },
+	},
+}
 
 -- Keybindings
 config.leader = { key = " ", mods = "CTRL", timeout_milliseconds = 2500 }
@@ -110,6 +214,7 @@ for i = 1, 9 do
 		action = act.ActivateTab(i - 1),
 	})
 end
+
 -- config.disable_default_key_bindings = true
 
 -- and finally, return the configuration to wezterm
